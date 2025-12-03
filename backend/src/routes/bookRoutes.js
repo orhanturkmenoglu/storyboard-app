3df1858a0e4ab97933b3c1e388671f0eb78b9690
orhan <orhantrkmn749@gmail.com>
2025-12-03 20:26:30 +0300

    feat: add book creation endpoint with Cloudinary image upload and JWT validation

---
+++ backend/src/routes/bookRoutes.js
@@ -0,0 +1,53 @@
+import express from "express";
+import cloudinary from "../config/cloudinary";
+import Book from "../models/Book";
+
+const router = express();
+
+router.post("/", async (req, res) => {
+  try {
+    const { title, caption, rating, image } = req.body;
+
+    console.log("📘 Create book request:", { title, rating });
+
+    // Required fields
+    if (!title || !caption || !rating || !image) {
+      console.log("❌ Missing fields");
+      return res.status(400).json({
+        success: false,
+        message: "Tüm alanlar zorunludur!",
+      });
+    }
+
+    // Upload image to Cloudinary
+    console.log("📤 Uploading image to Cloudinary...");
+
+    const uploadResponse = await cloudinary.uploader.upload(image);
+    console.log("UploadResponse :", uploadResponse);
+
+    const imageUrl = uploadResponse.secure_url;
+
+    const newBook = new Book({
+      title,
+      caption,
+      rating,
+      image: imageUrl,
+      user: req.userId,
+    });
+
+    await newBook.save();
+    console.log(`📚 New book created by user ${req.userId}`);
+
+    return res.status(201).json({
+      success: true,
+      message: "Book created successfully",
+      book,
+    });
+  } catch (error) {
+    console.error("❌ Book create error:", error);
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+});
