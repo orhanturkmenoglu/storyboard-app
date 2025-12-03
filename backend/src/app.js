e52842a41dfde1f40658cb275b18b9c9b74142a4
orhan <orhantrkmn749@gmail.com>
2025-12-03 19:56:05 +0300

    feat: add express app setup and auth route mounting

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,8 +4,13 @@ import "dotenv";
 
 const app = express();
 
+app.use(express.json());
+
 app.use("/api/auth",authRoutes);
 
+app.get("/", (req, res) => {
+  res.send("🚀 API is running...");
+});
 
 export default app;
 
