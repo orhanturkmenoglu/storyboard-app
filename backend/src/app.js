05e14d47b6a6d9afc2c2f863e739f2f64b2b6a5a
orhan <orhantrkmn749@gmail.com>
2025-12-04 18:40:16 +0300

    chore: setup CORS and register books router in app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,16 +1,19 @@
 import express from "express";
 import authRoutes from "./routes/authRoutes.js";
+import bookRoutes from "./routes/bookRoutes.js";
 import "dotenv";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
-app.use("/api/auth",authRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/books", bookRoutes);
 
 app.get("/", (req, res) => {
   res.send("🚀 API is running...");
 });
 
 export default app;
-
